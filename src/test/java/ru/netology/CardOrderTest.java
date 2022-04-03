df9243e1499487a1377f6b3325ff9c4e9eeff0e8
Tatyana Bondarenko <tanya_kursavka@mail.ru>
2022-04-03 22:58:09 +0300

    edited import

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -10,6 +10,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.*;
+
 public class CardOrderTest {
     @Test
     public void shouldOrderCardBySpecificDay() {
